8deaf04552df41333c77fb9f202f0f704658077a
Aditya Gurjar <17155247+adityadroid@users.noreply.github.com>
2023-10-19 20:04:45 +0530

    [T2] Improvements: animations for bottom loader in list

--- lib/core/ui/app_bottom_loader.dart
+++ lib/core/ui/app_bottom_loader.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:gripable_assignment/gen/assets.gen.dart';
+import 'package:lottie/lottie.dart';
 
 class AppBottomLoader extends StatelessWidget {
   const AppBottomLoader({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(8),
+    return Padding(
+      padding: const EdgeInsets.all(8),
       child: Center(
-        child: SizedBox(
-          height: 24,
-          width: 24,
-          child: CircularProgressIndicator(),
+        child: Lottie.asset(
+          Assets.animations.loadingAnimation,
+          height: 50,
+          width: 50,
         ),
       ),
     );
